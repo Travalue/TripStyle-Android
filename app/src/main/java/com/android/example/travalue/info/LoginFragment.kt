d7d9925e0a940d8c37dbf45a267668123422cdb5
mmihye <smilehye910@naver.com>
2022-12-30 21:27:54 +0900

    [#10] toolbar option add

--- app/src/main/java/com/android/example/travalue/info/LoginFragment.kt
+++ app/src/main/java/com/android/example/travalue/info/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.android.example.travalue.info
 
+import com.android.example.travalue.MainActivity
 import com.android.example.travalue.R
 import com.android.example.travalue.base.BaseFragment
 import com.android.example.travalue.databinding.FragmentLoginBinding
@@ -8,6 +9,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
 
     override fun initStartView() {
         super.initStartView()
+        (activity as MainActivity).setToolbarTitle("none")
     }
 
     override fun initDataBinding() {
