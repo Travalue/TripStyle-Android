d1d8873541ca46652ce9c15f6f91368c1dbf49cc
mmihye <smilehye910@naver.com>
2022-12-30 20:00:03 +0900

    [#8] traveller와 category 페이지 연결

---
+++ app/src/main/java/com/android/example/travalue/ui/traveller/TravellerFragment.kt
@@ -0,0 +1,27 @@
+package com.android.example.travalue.ui.traveller
+
+import androidx.viewpager2.widget.ViewPager2
+import com.android.example.travalue.R
+import com.android.example.travalue.base.BaseFragment
+import com.android.example.travalue.databinding.FragmentTravellerBinding
+import com.android.example.travalue.ui.trailer.TrailerViewPagerAdapter
+
+class TravellerFragment : BaseFragment<FragmentTravellerBinding>(R.layout.fragment_traveller) {
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+        // category 이동
+        binding.hambugerbar.setOnClickListener {
+            navController.navigate(R.id.action_travellerFragment_to_categoryFragment)
+        }
+    }
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+}
