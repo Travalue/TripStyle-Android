da3391f90292b08fd9aa182a6ef400ecb7bd4c93
mmihye <smilehye910@naver.com>
2023-01-31 19:21:55 +0900

    [#14] 프로필 편집 fragment 생성

---
+++ app/src/main/java/com/android/example/travalue/ui/mypage/EditProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.android.example.travalue.ui.mypage
+
+import com.android.example.travalue.MainActivity
+import com.android.example.travalue.R
+import com.android.example.travalue.base.BaseFragment
+import com.android.example.travalue.databinding.FragmentEditProfileBinding
+
+class EditProfileFragment  : BaseFragment<FragmentEditProfileBinding>(R.layout.fragment_edit_profile) {
+
+    override fun initStartView() {
+        super.initStartView()
+        (activity as MainActivity).setToolbarTitle("프로필 편집")
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+}
