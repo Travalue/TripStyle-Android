7edd8b9f2b6a788cb232bc12434efc1ecaa2c820
blonded99 <didls2654@naver.com>
2023-02-05 04:39:41 +0900

    [#16] 트레블러 탭에서 검색시 나타나는 결과창 우선 구현

--- app/src/main/java/com/android/example/travalue/ui/traveller/TrailerSearchFragment.kt
+++ app/src/main/java/com/android/example/travalue/ui/traveller/TrailerSearchFragment.kt
@@ -1,34 +1,36 @@
 package com.android.example.travalue.ui.traveller
 
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.android.example.travalue.MainActivity
 import com.android.example.travalue.R
 import com.android.example.travalue.base.BaseFragment
+import com.android.example.travalue.databinding.FragmentTrailerSearchBinding
 import com.android.example.travalue.databinding.FragmentTravellerBinding
 import com.android.example.travalue.ui.trailer.TrailerFragmentDirections
 import com.android.example.travalue.ui.trailer.TrailerViewPagerAdapter
 
-class TravellerFragment : BaseFragment<FragmentTravellerBinding>(R.layout.fragment_traveller) {
+class TrailerSearchFragment : BaseFragment<FragmentTrailerSearchBinding>(R.layout.fragment_trailer_search) {
     override fun initStartView() {
         super.initStartView()
-        (activity as MainActivity).setToolbarTitle("none")
+        (activity as MainActivity).setToolbarTitle("visible")
     }
 
     override fun initDataBinding() {
         super.initDataBinding()
 
         //recyclerView adapter
-        //binding.hotTraveller.adapter = TravellerRecyclerViewAdapter(context)
-        //binding.hotTraveller.layoutManager = LinearLayoutManager(context)
+        binding.trailerSearchResult.adapter = TrailerSearchRecyclerViewAdapter(context)
+        binding.trailerSearchResult.layoutManager = GridLayoutManager(context,3)
 
-        // category 이동 (임시)
-        binding.ivJeju.setOnClickListener {
-            navController.navigate(R.id.action_travellerFragment_to_TrailerSearchFragment)
+        // category 이동
+//        binding.hambugerbar.setOnClickListener {
+//            navController.navigate(R.id.action_travellerFragment_to_categoryDialogFragment)
 
 //            val action = TravellerFragmentDirections.actionTravellerFragmentToCategoryDialogFragment(binding.tvTraveller.text.toString())
 //            navController.navigate(action)
-        }
+//        }
     }
 
     override fun initAfterBinding() {
