0341491745ac5a95a74195d194342f6a221e3cca
zjaym1703 <jym971024@gmail.com>
2023-02-04 21:47:53 +0900

    [#15] timeline recyclerview 구현

--- app/src/main/java/com/android/example/travalue/ui/trailer/TrailerDetailFragment.kt
+++ app/src/main/java/com/android/example/travalue/ui/trailer/TrailerDetailFragment.kt
@@ -1,9 +1,12 @@
 package com.android.example.travalue.ui.trailer
 
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.example.travalue.MainActivity
 import com.android.example.travalue.R
 import com.android.example.travalue.base.BaseFragment
 import com.android.example.travalue.databinding.FragmentTrailerDetailBinding
+import com.android.example.travalue.util.ScheduleAdapter
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.OnMapReadyCallback
 
@@ -12,8 +15,9 @@ class TrailerDetailFragment : BaseFragment<FragmentTrailerDetailBinding>(R.layou
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).setToolbarTitle("trailer")
-        initMapView()
-
+        //initMapView()
+        animationScheduleView()
+        initScheduleView()
     }
 
     override fun initDataBinding() {
@@ -37,7 +41,47 @@ class TrailerDetailFragment : BaseFragment<FragmentTrailerDetailBinding>(R.layou
 
         mapFragment.getMapAsync(onMapReadyCallback)
     }
+    private fun animationScheduleView(){
+        binding.layoutScheduleTitle.setOnClickListener {
+            if(binding.layoutScheduleDetail.visibility == View.VISIBLE){
+                binding.layoutScheduleDetail.visibility = View.GONE
+                binding.scheduleButton.animate().apply {
+                    duration = 300
+                    rotation(0f)
+                }
+            }else{
+                val viewHeight = binding.layoutScheduleDetail.height
+                val scrollY = binding.scrollView.scrollY
 
+                binding.layoutScheduleDetail.visibility = View.VISIBLE
+                binding.scheduleButton.animate().apply {
+                    duration = 300
+                    rotation(180f)
+                }
+                binding.scrollView.post {
+                    binding.scrollView.smoothScrollTo(0,scrollY+viewHeight)
+                }
+
+            }
+        }
+    }
+
+    private fun initScheduleView(){
+        val adapter = ScheduleAdapter(getScheduleList())
+
+        binding.rvSchedule.addItemDecoration(adapter.ItemDecorator(-80))
+        binding.rvSchedule.adapter = ScheduleAdapter(getScheduleList())
+        binding.rvSchedule.layoutManager = LinearLayoutManager(context)
+    }
+
+    //mockup data
+    private fun getScheduleList(): ArrayList<String> {
+        return arrayListOf(
+            "순천 국가정원",
+            "순천 국가정원",
+            "순천 국가정원",
+            "순천 국가정원")
+    }
 
 
 }
