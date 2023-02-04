259ccca1b166191c91e3df9d5509b73849476811
zjaym1703 <jym971024@gmail.com>
2023-02-04 16:27:31 +0900

    [#15] 네이버 지도 API 추가

--- app/src/main/java/com/android/example/travalue/ui/trailer/TrailerDetailFragment.kt
+++ app/src/main/java/com/android/example/travalue/ui/trailer/TrailerDetailFragment.kt
@@ -3,13 +3,17 @@ package com.android.example.travalue.ui.trailer
 import com.android.example.travalue.MainActivity
 import com.android.example.travalue.R
 import com.android.example.travalue.base.BaseFragment
-import com.android.example.travalue.databinding.FragmentTrailerBinding
+import com.android.example.travalue.databinding.FragmentTrailerDetailBinding
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.OnMapReadyCallback
 
-class TrailerDetailFragment : BaseFragment<FragmentTrailerBinding>(R.layout.fragment_trailer_detail) {
+class TrailerDetailFragment : BaseFragment<FragmentTrailerDetailBinding>(R.layout.fragment_trailer_detail) {
 
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).setToolbarTitle("trailer")
+        initMapView()
+
     }
 
     override fun initDataBinding() {
@@ -19,5 +23,21 @@ class TrailerDetailFragment : BaseFragment<FragmentTrailerBinding>(R.layout.frag
     override fun initAfterBinding() {
         super.initAfterBinding()
     }
+
+    private fun initMapView(){
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment,it).commit()
+            }
+
+        val onMapReadyCallback = OnMapReadyCallback {
+
+        }
+
+        mapFragment.getMapAsync(onMapReadyCallback)
+    }
+
+
 
 }
