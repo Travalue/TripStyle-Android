c54fe4e9eb30ddd3ccb39a0877a30617e4a243e5
blonded99 <didls2654@naver.com>
2023-02-07 03:50:49 +0900

    [#16] 글 작성 화면 layout 우선 구현

--- app/src/main/java/com/android/example/travalue/ui/traveller/TravellerWriteFragment.kt
+++ app/src/main/java/com/android/example/travalue/ui/traveller/TravellerWriteFragment.kt
@@ -6,7 +6,7 @@ import com.android.example.travalue.R
 import com.android.example.travalue.base.BaseFragment
 import com.android.example.travalue.databinding.FragmentTrailerSearchBinding
 
-class TrailerSearchFragment : BaseFragment<FragmentTrailerSearchBinding>(R.layout.fragment_trailer_search) {
+class TravellerWriteFragment : BaseFragment<FragmentTrailerSearchBinding>(R.layout.fragment_trailer_search) {
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).setToolbarTitle("visible")
@@ -16,8 +16,8 @@ class TrailerSearchFragment : BaseFragment<FragmentTrailerSearchBinding>(R.layou
         super.initDataBinding()
 
         //recyclerView adapter
-        binding.trailerSearchResult.adapter = TrailerSearchRecyclerViewAdapter(context)
-        binding.trailerSearchResult.layoutManager = GridLayoutManager(context,3)
+//        binding.trailerSearchResult.adapter = TrailerSearchRecyclerViewAdapter(context)
+//        binding.trailerSearchResult.layoutManager = GridLayoutManager(context,3)
 
         // category 이동
 //        binding.hambugerbar.setOnClickListener {
