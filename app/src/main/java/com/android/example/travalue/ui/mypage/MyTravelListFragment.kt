45adaff98f21985dc767d5d2c33279890aa8d9f0
mmihye <smilehye910@naver.com>
2023-02-13 02:43:28 +0900

    [#33] 저장된 여행지 입력시 여행지 추가 활성화

--- app/src/main/java/com/android/example/travalue/ui/mypage/MyTravelListFragment.kt
+++ app/src/main/java/com/android/example/travalue/ui/mypage/MyTravelListFragment.kt
@@ -1,15 +1,27 @@
 package com.android.example.travalue.ui.mypage
 
+import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.android.example.travalue.MainActivity
 import com.android.example.travalue.R
 import com.android.example.travalue.base.BaseFragment
 import com.android.example.travalue.databinding.FragmentMytravelListBinding
+import java.util.regex.Pattern
 
 class MyTravelListFragment : BaseFragment<FragmentMytravelListBinding>(R.layout.fragment_mytravel_list) {
+
+    private var allPlace :ArrayList<String> =  ArrayList()
+    private var placeIcon :ArrayList<String> = ArrayList()
 
     override fun initStartView() {
         super.initStartView()
         (activity as MainActivity).setToolbarTitle("나의 여행지 리스트")
+
+        allPlace.add("미국")
+        placeIcon.add("🇺🇸")
     }
 
     override fun initDataBinding() {
@@ -22,5 +34,38 @@ class MyTravelListFragment : BaseFragment<FragmentMytravelListBinding>(R.layout.
     override fun initAfterBinding() {
         super.initAfterBinding()
 
+
+        binding.etTravel.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+//                val place = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO("not implemented")
+                // To change body of created functions use File | Settings | File Templates.
+            }
+
+            @SuppressLint("ResourceAsColor")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val place = binding.etTravel.text.toString()
+
+                // 저장된 여행지를 입력했을때
+                if(allPlace.contains(place)){
+                    binding.btnAddTravel.isEnabled=true
+                    binding.btnAddTravel.setTextColor(ContextCompat.getColor(context!!, R.color.black))
+                    binding.ivIcon.visibility = View.GONE
+                    binding.tvAddIcon.visibility=View.GONE
+                    binding.tvPlaceIcon.text = placeIcon[0]
+                    binding.tvPlaceIcon.visibility=View.VISIBLE
+                }else{
+                    binding.btnAddTravel.isEnabled=false
+                    binding.btnAddTravel.setTextColor(ContextCompat.getColor(context!!, R.color.gray))
+                    binding.ivIcon.visibility = View.VISIBLE
+                    binding.tvAddIcon.visibility=View.VISIBLE
+                    binding.tvPlaceIcon.visibility=View.GONE
+                }
+
+            }
+        })
     }
 }
