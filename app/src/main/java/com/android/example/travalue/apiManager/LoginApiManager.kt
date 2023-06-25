a8c3596c8a1946685ebca0873f833708cac9b517
mmihye <smilehye910@naver.com>
2023-06-25 23:26:34 +0900

    [#48] 추가 변경사항

--- app/src/main/java/com/android/example/travalue/apiManager/LoginApiManager.kt
+++ app/src/main/java/com/android/example/travalue/apiManager/LoginApiManager.kt
@@ -30,7 +30,7 @@ class LoginApiManager {
 
     init {
         retrofit = Retrofit.Builder()
-            .baseUrl("http://13.125.137.16:8080")
+            .baseUrl("http://13.125.137.16:8080/v1/auth/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
