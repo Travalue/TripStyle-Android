389352eb2c2aa150868d9e6c5a79263594cae36f
yydev10 <jym971024@gmail.com>
2023-06-28 19:13:25 +0900

    [#47] 패키지 변경 오류 수정

--- app/src/main/java/com/tripstyle/tripstyle/network/MapService.kt
+++ app/src/main/java/com/tripstyle/tripstyle/network/MapService.kt
@@ -1,7 +1,7 @@
 package com.tripstyle.tripstyle.network
 
 import com.tripstyle.tripstyle.BuildConfig
-import com.tripstyle.tripstyle.network.res.SearchResult
+import com.tripstyle.tripstyle.model.SearchResult
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
