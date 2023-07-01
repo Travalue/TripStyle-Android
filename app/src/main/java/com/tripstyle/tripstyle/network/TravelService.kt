7f852a70b82ed715ee18f65846264dade4d15d28
yydev10 <jym971024@gmail.com>
2023-07-01 16:28:08 +0900

    [#56] 트레일러 상세조회 suburl 추가

--- app/src/main/java/com/tripstyle/tripstyle/network/TravelService.kt
+++ app/src/main/java/com/tripstyle/tripstyle/network/TravelService.kt
@@ -1,12 +1,19 @@
 package com.tripstyle.tripstyle.network
 
 import com.tripstyle.tripstyle.model.TrailerResponseModel
+import com.tripstyle.tripstyle.model.TravelDetailResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TravelService {
 
     //Trailer 전체조회
     @GET("/post/trailer")
     fun getTrailerList() : Call<TrailerResponseModel>
+
+    // Trailer, Travler 상세조회
+    @GET("/post/{id}")
+    fun getTravelPost(@Path("id")id:Int) : Call<TravelDetailResponse>
+
 }
