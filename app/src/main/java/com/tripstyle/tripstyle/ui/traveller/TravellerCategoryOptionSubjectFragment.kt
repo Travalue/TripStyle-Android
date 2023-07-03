e85feae59fc2fbdc53003f3675d6ea8ceba7d236
blonded99 <didls2654@naver.com>
2023-07-03 23:46:09 +0900

    [#43] 트레블러 카테고리 옵션 주제 선택 기능 구현
    트레블러 카테고리 옵션 주제 선택 화면에서 주제를 선택하면 이전 화면으로 돌아오고 사용자가 선택한 주제가 화면에 표시됨

--- app/src/main/java/com/tripstyle/tripstyle/ui/traveller/TravellerCategoryOptionSubjectFragment.kt
+++ app/src/main/java/com/tripstyle/tripstyle/ui/traveller/TravellerCategoryOptionSubjectFragment.kt
@@ -1,11 +1,14 @@
 package com.tripstyle.tripstyle.ui.traveller
 
+import androidx.fragment.app.activityViewModels
 import com.tripstyle.tripstyle.R
 import com.tripstyle.tripstyle.base.BaseFragment
 import com.tripstyle.tripstyle.MainActivity
 import com.tripstyle.tripstyle.databinding.FragmentTravellerCategoryOptionSubjectBinding
 
 class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCategoryOptionSubjectBinding>(R.layout.fragment_traveller_category_option_subject) {
+
+    private val viewModel by activityViewModels<TravellerWriteViewModel>()
 
     override fun initStartView() {
         super.initStartView()
@@ -16,6 +19,12 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
         super.initDataBinding()
         binding.tvSubjectCategorySelected.setOnClickListener {
             navController.navigate(R.id.action_categoryOptionSubjectFragment_to_categoryOptionSubjectSelectFragment)
+        }
+
+        // 사용자가 선택한 카테고리 주제
+        viewModel.categorySubjectLiveData.observe(viewLifecycleOwner){
+            if(!it.isNullOrBlank())
+                binding.tvSubjectCategorySelected.text = it
         }
 
     }
