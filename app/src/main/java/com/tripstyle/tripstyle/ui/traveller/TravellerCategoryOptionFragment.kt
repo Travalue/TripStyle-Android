5ffb53313cf274148a5c6bfc710766525e34ab19
blonded99 <didls2654@naver.com>
2023-07-06 00:17:56 +0900

    [#43] 트레블러 카테고리 옵션 공개 설정 구현

--- app/src/main/java/com/tripstyle/tripstyle/ui/traveller/TravellerCategoryOptionFragment.kt
+++ app/src/main/java/com/tripstyle/tripstyle/ui/traveller/TravellerCategoryOptionFragment.kt
@@ -1,5 +1,6 @@
 package com.tripstyle.tripstyle.ui.traveller
 
+import android.view.View
 import com.tripstyle.tripstyle.R
 import com.tripstyle.tripstyle.base.BaseFragment
 import com.tripstyle.tripstyle.MainActivity
@@ -17,11 +18,29 @@ class TravellerCategoryOptionFragment : BaseFragment<FragmentTravellerCategoryOp
         binding.tvCategoryAdd.setOnClickListener {
             navController.navigate(R.id.action_categoryOptionFragment_to_categoryOptionSubjectFragment)
         }
+
+        binding.radioGroupOpenStatus.setOnCheckedChangeListener{ group, checkedId ->
+            when(checkedId){
+                R.id.button_public -> changeOpenStatusTextView(true)
+                R.id.button_private -> changeOpenStatusTextView(false)
+            }
+        }
     }
 
     override fun initAfterBinding() {
         super.initAfterBinding()
 
+    }
+
+    private fun changeOpenStatusTextView(isPublic: Boolean){
+        if(isPublic) {
+            binding.tvOpenStatusPrivate.visibility = View.INVISIBLE
+            binding.tvOpenStatusPublic.visibility = View.VISIBLE
+        }
+        else{
+            binding.tvOpenStatusPublic.visibility = View.INVISIBLE
+            binding.tvOpenStatusPrivate.visibility = View.VISIBLE
+        }
     }
 
 }
