8eb78d4bbb910c9de5f8e8c37c61fc0d2e8b28f5
yydev10 <jym971024@gmail.com>
2023-07-13 19:53:07 +0900

    [#81] 커버 이미지 추가 기능 구현

--- app/src/main/java/com/tripstyle/tripstyle/presentation/ui/mypage/CategoryEditFragment.kt
+++ app/src/main/java/com/tripstyle/tripstyle/presentation/ui/mypage/CategoryEditFragment.kt
@@ -1,20 +1,45 @@
 package com.tripstyle.tripstyle.presentation.ui.mypage
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import android.view.*
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
+import com.bumptech.glide.Glide
 import com.tripstyle.tripstyle.dialog.CategoryDeleteDialog
 import com.tripstyle.tripstyle.R
 import com.tripstyle.tripstyle.base.BaseFragment
+import com.tripstyle.tripstyle.data.model.dto.BaseResponseModel
+import com.tripstyle.tripstyle.data.model.dto.CategoryUpdateRequest
+import com.tripstyle.tripstyle.data.source.remote.CategoryService
 import com.tripstyle.tripstyle.databinding.FragmentCategoryEditBinding
+import com.tripstyle.tripstyle.di.AppClient
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
 
 
 class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout.fragment_category_edit) {
+
+    private lateinit var subjectCategory : String
+    private lateinit var locationCategory : String
+    private lateinit var imageUri : Uri
 
     override fun initStartView() {
         super.initStartView()
@@ -22,7 +47,7 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
         initMenu()
         initCategorySpinner()
         initLocationSpinner()
-        clickButtonEvent()
+
     }
 
     override fun initDataBinding() {
@@ -31,6 +56,8 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
 
     override fun initAfterBinding() {
         super.initAfterBinding()
+        clickButtonEvent()
+        selectCoverImage()
     }
 
     // 메뉴 초기화
@@ -45,7 +72,7 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when (menuItem.itemId) {
                     R.id.menu_confrim_button -> {
-                        Toast.makeText(context,"수정 되었습니다",Toast.LENGTH_SHORT).show()
+                        requestUpdateCategory(1,binding.etCategory.text.toString(),subjectCategory,locationCategory)
                         true
                     }
                     else -> false
@@ -61,7 +88,7 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
         binding.spinnerSubject.adapter = adapter
         binding.spinnerSubject.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, v: View?, position: Int, id: Long) {
-                Log.d("selected item",data[position])
+                subjectCategory = data[position]
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -78,7 +105,7 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
         binding.spinnerLocation.adapter = adapter
         binding.spinnerLocation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, v: View?, position: Int, id: Long) {
-                Log.d("selected item",data[position])
+                locationCategory = data[position]
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -88,6 +115,40 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
         }
     }
 
+    //커버이미지 선택
+    private fun selectCoverImage(){
+        binding.ivIcon.setOnClickListener {
+            selectGallery()
+        }
+    }
+
+    //network 연결
+    private fun requestUpdateCategory(categoryId:Int,title:String,subject:String,region:String){
+        val service = AppClient.retrofit?.create(CategoryService::class.java)
+        val image = setImagePart(imageUri)
+        val requestModel = CategoryUpdateRequest(title = title, subject =subject, region = region, thumbnail =image)
+        service?.updateCategory(categoryId,requestModel)?.enqueue(object : Callback<BaseResponseModel> {
+            override fun onResponse(
+                call: Call<BaseResponseModel>,
+                response: Response<BaseResponseModel>
+            ) {
+                if(response.body()?.code==201){
+                    Log.d("response",response.body().toString())
+                }else{
+                    Toast.makeText(requireContext(),"카테고리 수정에 오류가 발생했습니다",Toast.LENGTH_SHORT).show()
+                    Log.d("response",response.body().toString())
+                }
+
+            }
+
+            override fun onFailure(call: Call<BaseResponseModel>, t: Throwable) {
+                Log.d("data","네트워크 연결 실패 : ${t}")
+            }
+
+        })
+    }
+
+
     //카테고리 삭제 버튼 이벤트
     private fun clickButtonEvent(){
         binding.btnCategoryDelete.setOnClickListener {
@@ -95,4 +156,63 @@ class CategoryEditFragment :  BaseFragment<FragmentCategoryEditBinding>(R.layout
         }
     }
 
+    companion object{
+        const val REQ_GALLERY = 1
+    }
+
+
+    private val imageResultSingle = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            result?.data?.let { it ->
+                imageUri = it.data!!
+                val image = getRealPathFromURI(it.data!!)
+                refreshBackgroundImage(image)
+            }
+        }
+    }
+
+    private fun getRealPathFromURI(uri: Uri): String{
+        var columnIndex = 0
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = activity?.contentResolver?.query(uri, proj, null, null, null)
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        val result = cursor.getString(columnIndex)
+        cursor.close()
+        return result
+    }
+
+
+    private fun refreshBackgroundImage(imageUri: String){
+        context?.let {
+            Glide.with(it).load(imageUri)
+                .centerCrop()
+                .into(binding.ivIcon)
+        }
+    }
+
+
+    private fun selectGallery(){
+        val readPermission = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if(readPermission == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            ),
+                REQ_GALLERY
+            )
+        }else{
+            var intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*")
+            imageResultSingle.launch(intent)
+        }
+    }
+
+    private fun setImagePart(fileUri: Uri): MultipartBody.Part {
+        val file = File(fileUri.path)
+        val requestFile =  RequestBody.create("image/*".toMediaTypeOrNull(),file)
+        return MultipartBody.Part.createFormData("image", file.name, requestFile)
+    }
 }
