254b4edf6f9207b6e9dd7ad919508045d28accc3
blonded99 <didls2654@naver.com>
2023-07-16 03:33:29 +0900

    [#80] 카테고리 추가 관련 Retrofit 서비스 정의 추가

--- app/src/main/java/com/tripstyle/tripstyle/data/source/remote/TravelService.kt
+++ app/src/main/java/com/tripstyle/tripstyle/data/source/remote/TravelService.kt
@@ -2,16 +2,11 @@ package com.tripstyle.tripstyle.data.source.remote
 
 
 import com.tripstyle.tripstyle.BuildConfig
-import com.tripstyle.tripstyle.data.model.dto.BaseResponseModel
-import com.tripstyle.tripstyle.data.model.dto.HotTravellerResponse
-import com.tripstyle.tripstyle.data.model.dto.TrailerResponse
-import com.tripstyle.tripstyle.data.model.dto.TravelDetailResponse
+import com.tripstyle.tripstyle.data.model.dto.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface TravelService {
 
@@ -43,5 +38,17 @@ interface TravelService {
     // 지금 핫한 트레블러 조회
     @GET("/post/traveller/hot")
     fun getHotTravellerList() : Call<HotTravellerResponse>
+
+    // 카테고리 추가
+    @Headers("Authorization: ${BuildConfig.TOKEN}")
+    @Multipart
+    @POST("/category")
+    fun addCategory(
+        @Part thumbnail: MultipartBody.Part,
+        @Part("subject") subject: RequestBody,
+        @Part("title") title: RequestBody,
+        @Part("region") region: RequestBody,
+        @Part("userId") userId: RequestBody
+    ): Call<CategoryAddResponse>
 
 }
