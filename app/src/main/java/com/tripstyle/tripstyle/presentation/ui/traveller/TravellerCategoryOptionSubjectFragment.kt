fb5664a89ad72d618a21cb6fd5500b835e413b1b
blonded99 <didls2654@naver.com>
2023-07-18 08:50:25 +0900

    [#80] 카테고리 추가 화면 툴바 메뉴 조건부 활성화/비활성화 구현
    함수 위치 조정

--- app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerCategoryOptionSubjectFragment.kt
+++ app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerCategoryOptionSubjectFragment.kt
@@ -22,6 +22,7 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.tripstyle.tripstyle.R
 import com.tripstyle.tripstyle.base.BaseFragment
@@ -29,6 +30,8 @@ import com.tripstyle.tripstyle.data.model.dto.CategoryAddResponse
 import com.tripstyle.tripstyle.data.source.remote.TravelService
 import com.tripstyle.tripstyle.databinding.FragmentTravellerCategoryOptionSubjectBinding
 import com.tripstyle.tripstyle.di.AppClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -47,37 +50,37 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
     var isImageUploaded = false
     var categoryCoverImageUri = ""
 
-    companion object{
+    companion object {
         const val REQ_GALLERY = 1
     }
 
-    private val imageResultSingle = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-        if(result.resultCode == Activity.RESULT_OK){
-            result?.data?.let { it ->
-                if (it.clipData != null) {   // 사진 여러장 선택
-                    val count = it.clipData!!.itemCount
-                    if (count > 1) {
-                        // 아래 toast를 다른 표시 방법으로 변경할 것
-                        Toast.makeText(context, "사진은 1장만 선택 가능합니다.", Toast.LENGTH_SHORT)
-                            .show()
-                        return@registerForActivityResult
-                    }
+    private val imageResultSingle =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result?.data?.let { it ->
+                    if (it.clipData != null) {   // 사진 여러장 선택
+                        val count = it.clipData!!.itemCount
+                        if (count > 1) {
+                            // 아래 toast를 다른 표시 방법으로 변경할 것
+                            Toast.makeText(context, "사진은 1장만 선택 가능합니다.", Toast.LENGTH_SHORT)
+                                .show()
+                            return@registerForActivityResult
+                        }
 
-                    for (i in 0 until count) {
-                        val imageUri = getRealPathFromURI(it.clipData!!.getItemAt(i).uri)
+                        for (i in 0 until count) {
+                            val imageUri = getRealPathFromURI(it.clipData!!.getItemAt(i).uri)
+                            refreshCategoryCoverImage(imageUri) // 카테고리 커버 이미지 변경
+                            categoryCoverImageUri = imageUri
+                        }
+                    } else {    // 사진 1장 선택
+                        val imageUri = getRealPathFromURI(it.data!!)
                         refreshCategoryCoverImage(imageUri) // 카테고리 커버 이미지 변경
                         categoryCoverImageUri = imageUri
                     }
-                } else {    // 사진 1장 선택
-                    val imageUri = getRealPathFromURI(it.data!!)
-                    refreshCategoryCoverImage(imageUri) // 카테고리 커버 이미지 변경
-                    categoryCoverImageUri = imageUri
-                }
 
+                }
             }
         }
-    }
 
     override fun initStartView() {
         super.initStartView()
@@ -110,8 +113,8 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
         }
 
         // 사용자가 선택한 카테고리 주제
-        viewModel.categorySubjectLiveData.observe(viewLifecycleOwner){
-            if(!it.isNullOrBlank()) {
+        viewModel.categorySubjectLiveData.observe(viewLifecycleOwner) {
+            if (!it.isNullOrBlank()) {
                 binding.tvSubjectCategorySelected.text = it
                 checkFields()
             }
@@ -124,7 +127,7 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
         // (임시) 카테고리 추가 버튼
         // TODO: 툴바로 바꿀 것
         binding.btnAddCategory.setOnClickListener {
-            Log.e("","add button clicked")
+            Log.e("", "add button clicked")
             requestAddCategory()
             navController.popBackStack()
         }
@@ -136,26 +139,33 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
 
     }
 
-    private fun selectGallery(){
-        val readPermission = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
-        val writePermission = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private fun selectGallery() {
+        val readPermission = ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        val writePermission = ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
 
-        if(readPermission == PackageManager.PERMISSION_DENIED || writePermission == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ),
+        if (readPermission == PackageManager.PERMISSION_DENIED || writePermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
                 REQ_GALLERY
             )
-        }else{
-             // 사진 1장만 선택
+        } else {
+            // 사진 1장만 선택
             var intent = Intent(Intent.ACTION_PICK)
-            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*")
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
             imageResultSingle.launch(intent)
         }
     }
 
-    private fun refreshCategoryCoverImage(imageUri: String){
+    private fun refreshCategoryCoverImage(imageUri: String) {
         context?.let {
             Glide.with(it).load(imageUri)
                 .centerCrop()
@@ -165,11 +175,11 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
         checkFields(true)
     }
 
-    private fun getRealPathFromURI(uri: Uri): String{
+    private fun getRealPathFromURI(uri: Uri): String {
         var columnIndex = 0
         val proj = arrayOf(MediaStore.Images.Media.DATA)
         val cursor = activity?.contentResolver?.query(uri, proj, null, null, null)
-        if(cursor!!.moveToFirst()){
+        if (cursor!!.moveToFirst()) {
             columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
         }
         val result = cursor.getString(columnIndex)
@@ -177,50 +187,45 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
         return result
     }
 
-    // 카테고리 추가하기 전 모든 필드값이 채워졌는지 확인하고 버튼 활성화
-    fun checkFields(isImageUploadedNow: Boolean = false) {
-        if (isImageUploadedNow) {
-            binding.btnAddCategory.isEnabled =
-                binding.editTextCategoryName.text.trim().isNotEmpty() &&
-                        binding.editTextRegion.text.trim().isNotEmpty() &&
-                        binding.tvSubjectCategorySelected.text.trim().isNotEmpty()
+
+    /* 툴바 메뉴 관련 */
+
+    private fun setMenuTextViewEnabled(enabled: Boolean) {
+        if (enabled) {
+            // 메뉴(등록) 활성화
+            menuTextView.isEnabled = true
+            menuTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
         } else {
-            binding.btnAddCategory.isEnabled =
-                binding.editTextCategoryName.text.trim().isNotEmpty() &&
-                        binding.editTextRegion.text.trim().isNotEmpty() &&
-                        binding.tvSubjectCategorySelected.text.trim().isNotEmpty() &&
-                        isImageUploaded
+            // 비활성화
+            menuTextView.isEnabled = false
+            menuTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_959595))
         }
     }
 
-    private fun requestAddCategory(){
-        val file = File(categoryCoverImageUri)
-        val thumbnail = MultipartBody.Part.createFormData("thumbnail", file.name, file.asRequestBody())
-        val subject = binding.tvSubjectCategorySelected.text.toString()
-            .toRequestBody("text/plain".toMediaTypeOrNull())
-        val title = binding.editTextCategoryName.text.toString()
-            .toRequestBody("text/plain".toMediaTypeOrNull())
-        val region = binding.editTextRegion.text.toString()
-            .toRequestBody("text/plain".toMediaTypeOrNull())
-        //  TODO: userId 변경 필요
-        val userId = "1".toRequestBody("text/plain".toMediaTypeOrNull())
-
-        val service = AppClient.retrofit?.create(TravelService::class.java)
-
-        service?.addCategory(thumbnail, subject, title, region, userId)?.enqueue(object : Callback<CategoryAddResponse> {
-            override fun onResponse(call: Call<CategoryAddResponse>, response: Response<CategoryAddResponse>) {
-                if (response.isSuccessful) {
-                    Log.e("AddCategoryResponse:", response.body().toString())
-                } else {
-                    Log.e("AddCategoryResponse:", "응답 실패")
-                }
+    // 카테고리 추가하기 전 모든 필드값이 채워졌는지 확인하고 버튼 활성화
+    fun checkFields(isImageUploadedNow: Boolean = false) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            if (isImageUploadedNow) {
+                if (
+                    binding.editTextCategoryName.text.trim().isNotEmpty() &&
+                    binding.editTextRegion.text.trim().isNotEmpty() &&
+                    binding.tvSubjectCategorySelected.text.trim().isNotEmpty()
+                )
+                    setMenuTextViewEnabled(true)
+                else
+                    setMenuTextViewEnabled(false)
+            } else {
+                if (
+                    binding.editTextCategoryName.text.trim().isNotEmpty() &&
+                    binding.editTextRegion.text.trim().isNotEmpty() &&
+                    binding.tvSubjectCategorySelected.text.trim().isNotEmpty() &&
+                    isImageUploaded
+                )
+                    setMenuTextViewEnabled(true)
+                else
+                    setMenuTextViewEnabled(false)
             }
-
-            override fun onFailure(call: Call<CategoryAddResponse>, t: Throwable) {
-                Log.e("AddCategoryResponse:", "요청 실패", t)
-            }
-        })
-
+        }
     }
 
     private fun initMenu(){
@@ -251,6 +256,46 @@ class TravellerCategoryOptionSubjectFragment : BaseFragment<FragmentTravellerCat
                 return false
             }
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+
+
+
+    /* API CALL */
+
+    private fun requestAddCategory() {
+        val file = File(categoryCoverImageUri)
+        val thumbnail =
+            MultipartBody.Part.createFormData("thumbnail", file.name, file.asRequestBody())
+        val subject = binding.tvSubjectCategorySelected.text.toString()
+            .toRequestBody("text/plain".toMediaTypeOrNull())
+        val title = binding.editTextCategoryName.text.toString()
+            .toRequestBody("text/plain".toMediaTypeOrNull())
+        val region = binding.editTextRegion.text.toString()
+            .toRequestBody("text/plain".toMediaTypeOrNull())
+        //  TODO: userId 변경 필요
+        val userId = "1".toRequestBody("text/plain".toMediaTypeOrNull())
+
+        val service = AppClient.retrofit?.create(TravelService::class.java)
+
+        service?.addCategory(thumbnail, subject, title, region, userId)
+            ?.enqueue(object : Callback<CategoryAddResponse> {
+                override fun onResponse(
+                    call: Call<CategoryAddResponse>,
+                    response: Response<CategoryAddResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.e("AddCategoryResponse:", response.body().toString())
+                    } else {
+                        Log.e("AddCategoryResponse:", "응답 실패")
+                    }
+                }
+
+                override fun onFailure(call: Call<CategoryAddResponse>, t: Throwable) {
+                    Log.e("AddCategoryResponse:", "요청 실패", t)
+                }
+            })
+
     }
 
 }
