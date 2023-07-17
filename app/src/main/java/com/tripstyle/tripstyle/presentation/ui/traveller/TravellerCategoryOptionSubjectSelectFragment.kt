a9d923525a82baf9c8fca18d5fdd7577471143d1
blonded99 <didls2654@naver.com>
2023-07-18 07:14:38 +0900

    [#80] 주제 분류 화면에서 툴바 메뉴를 활용하도록 변경
    최초에 Default로 '식도락' RadioButton이 체크되도록 함
    저장 버튼을 누르지 않으면 선택 사항이 반영되지 않으며, 한 번 저장 버튼을 눌러 카테고리 추가 화면으로 돌아간 다음 다시 주제 분류 화면으로 진입할 때는 기존에 사용자가 설정한 주제에 버튼이 선택되어 있도록 함

--- app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerCategoryOptionSubjectSelectFragment.kt
+++ app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerCategoryOptionSubjectSelectFragment.kt
@@ -1,6 +1,5 @@
 package com.tripstyle.tripstyle.presentation.ui.traveller
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -12,33 +11,23 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import com.tripstyle.tripstyle.R
 import com.tripstyle.tripstyle.base.BaseFragment
-import com.tripstyle.tripstyle.MainActivity
 import com.tripstyle.tripstyle.databinding.FragmentTravellerCategoryOptionSubjectSelectBinding
 
 class TravellerCategoryOptionSubjectSelectFragment : BaseFragment<FragmentTravellerCategoryOptionSubjectSelectBinding>(R.layout.fragment_traveller_category_option_subject_select) {
 
     private val viewModel by activityViewModels<TravellerWriteViewModel>()
-
     private lateinit var menuTextView: TextView
 
     override fun initStartView() {
         super.initStartView()
-//        (activity as MainActivity).setToolbarTitle("")
+
         initMenu()
     }
 
     override fun initDataBinding() {
         super.initDataBinding()
 
-        binding.radiogroupCategorySubject.setOnCheckedChangeListener{ group, checkedId ->
-            when(checkedId){
-                R.id.button_epicurism -> backToPreviousScreen(binding.buttonEpicurism.text.toString())
-                R.id.button_activity -> backToPreviousScreen(binding.buttonActivity.text.toString())
-                R.id.button_rest -> backToPreviousScreen(binding.buttonRest.text.toString())
-                R.id.button_package -> backToPreviousScreen(binding.buttonPackage.text.toString())
-            }
-        }
-
+        initRadioButton()
     }
 
     override fun initAfterBinding() {
@@ -46,8 +35,25 @@ class TravellerCategoryOptionSubjectSelectFragment : BaseFragment<FragmentTravel
 
     }
 
-    private fun backToPreviousScreen(subject: String){
-        viewModel.updateCategorySubject(subject)
+    // RadioButton 초기 상태 설정
+    private fun initRadioButton(){
+        when(viewModel.categorySubjectLiveData.value){
+            "식도락" -> binding.buttonEpicurism.isChecked = true
+            "액티비티" -> binding.buttonActivity.isChecked = true
+            "휴양" -> binding.buttonRest.isChecked = true
+            "패키지" -> binding.buttonPackage.isChecked = true
+            else -> binding.buttonEpicurism.isChecked = true
+        }
+    }
+
+    // 현재 선택된 RadioButton에 따라 ViewModel을 업데이트하고 이전 화면으로 이동
+    private fun saveStatusAndBackToPreviousScreen(){
+        when(binding.radiogroupCategorySubject.checkedRadioButtonId){
+            R.id.button_epicurism -> viewModel.updateCategorySubject(binding.buttonEpicurism.text.toString())
+            R.id.button_activity -> viewModel.updateCategorySubject(binding.buttonActivity.text.toString())
+            R.id.button_rest -> viewModel.updateCategorySubject(binding.buttonRest.text.toString())
+            R.id.button_package -> viewModel.updateCategorySubject(binding.buttonPackage.text.toString())
+        }
         navController.popBackStack()
     }
 
@@ -69,7 +75,7 @@ class TravellerCategoryOptionSubjectSelectFragment : BaseFragment<FragmentTravel
                 menuTextView.setOnClickListener {
                     when (menuItem.itemId) {
                         R.id.menu_traveller_btn_save -> {
-                            Log.e("","Category Subject Save Button Clicked")
+                            saveStatusAndBackToPreviousScreen()
                         }
                     }
                 }
