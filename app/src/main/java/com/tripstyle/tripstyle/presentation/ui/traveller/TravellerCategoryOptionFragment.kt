4c118ae96f34a8145a26b651f81f8cac27e121f9
blonded99 <didls2654@naver.com>
2023-07-20 00:29:36 +0900

    [#80] 카테고리 전체 조회 API 호출

--- app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerCategoryOptionFragment.kt
+++ app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerCategoryOptionFragment.kt
@@ -1,16 +1,21 @@
 package com.tripstyle.tripstyle.presentation.ui.traveller
 
+import android.util.Log
 import android.view.View
 import com.tripstyle.tripstyle.R
 import com.tripstyle.tripstyle.base.BaseFragment
-import com.tripstyle.tripstyle.MainActivity
+import com.tripstyle.tripstyle.data.model.dto.CategoryReadResponse
+import com.tripstyle.tripstyle.data.source.remote.TravelService
 import com.tripstyle.tripstyle.databinding.FragmentTravellerCategoryOptionBinding
+import com.tripstyle.tripstyle.di.AppClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TravellerCategoryOptionFragment : BaseFragment<FragmentTravellerCategoryOptionBinding>(R.layout.fragment_traveller_category_option) {
 
     override fun initStartView() {
         super.initStartView()
-//        (activity as MainActivity).setToolbarTitle("")
     }
 
     override fun initDataBinding() {
@@ -30,6 +35,7 @@ class TravellerCategoryOptionFragment : BaseFragment<FragmentTravellerCategoryOp
     override fun initAfterBinding() {
         super.initAfterBinding()
 
+        requestCategoryList()
     }
 
     private fun changeOpenStatusTextView(isPublic: Boolean){
@@ -41,6 +47,29 @@ class TravellerCategoryOptionFragment : BaseFragment<FragmentTravellerCategoryOp
             binding.tvOpenStatusPublic.visibility = View.INVISIBLE
             binding.tvOpenStatusPrivate.visibility = View.VISIBLE
         }
+    }
+
+    private fun requestCategoryList() {
+        val service = AppClient.retrofit?.create(TravelService::class.java)
+
+        // TODO: userId 부분 변경 필요
+        service?.getCategoryList(1)?.enqueue(object : Callback<CategoryReadResponse> {
+            override fun onResponse(
+                call: Call<CategoryReadResponse>,
+                response: Response<CategoryReadResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val categoryList = response.body()?.data
+                    Log.e("CategoryReadResponse", "categoryList: ${categoryList.toString()}")
+                } else {
+                    Log.e("CategoryReadResponse", "Server error, CODE: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<CategoryReadResponse>, t: Throwable) {
+                Log.e("CategoryReadResponse", "requestCategoryList API CALL FAILED")
+            }
+        })
     }
 
 }
