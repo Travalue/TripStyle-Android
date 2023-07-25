7add09d7240ca83f232c9e925bc7c9383c5589bf
blonded99 <didls2654@naver.com>
2023-07-25 19:08:56 +0900

    [#89] 트레블러 메인 검색 관련 adapter 파일명 변경
    검색 관련 RecyclerView Adapter 파일명 변경

--- app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerSearchDomesticAdapter.kt
+++ app/src/main/java/com/tripstyle/tripstyle/presentation/ui/traveller/TravellerSearchDomesticAdapter.kt
@@ -11,8 +11,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tripstyle.tripstyle.R
 import com.bumptech.glide.Glide
 
-class TravellerSearchRecyclerViewAdapter2(private val viewModel: TravellerSearchViewModel, val context: Context?):
-    RecyclerView.Adapter<TravellerSearchRecyclerViewAdapter2.RecyclerViewViewHolder>() {
+class TravellerSearchDomesticAdapter(private val viewModel: TravellerSearchViewModel, val context: Context?):
+    RecyclerView.Adapter<TravellerSearchDomesticAdapter.RecyclerViewViewHolder>() {
 
 
     interface OnItemClickListener{
@@ -24,7 +24,6 @@ class TravellerSearchRecyclerViewAdapter2(private val viewModel: TravellerSearch
     fun setOnItemClickListener(listener: OnItemClickListener){
         this.onClickListener = listener
     }
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.traveller_search_item_view, parent, false)
@@ -38,7 +37,7 @@ class TravellerSearchRecyclerViewAdapter2(private val viewModel: TravellerSearch
 
     override fun getItemCount(): Int {
         // 검색결과에 표시될 item 개수
-        return viewModel.overseasCities.size
+        return viewModel.domesticCities.size
     }
 
     inner class RecyclerViewViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -46,7 +45,6 @@ class TravellerSearchRecyclerViewAdapter2(private val viewModel: TravellerSearch
         private val QueryCityName: TextView = itemView.findViewById(R.id.city_name)
         private val QuerySpecificCityName: TextView = itemView.findViewById(R.id.specific_city_name)
         private val searchButton: ImageButton = itemView.findViewById(R.id.btn_search_result)
-
 
         init{
             itemView.setOnClickListener {
@@ -60,20 +58,17 @@ class TravellerSearchRecyclerViewAdapter2(private val viewModel: TravellerSearch
             }
         }
 
-
-
         fun setContents(pos: Int){
-            with(viewModel.overseasCities[pos]){
+            with(viewModel.domesticCities[pos]){
                 //세팅
                 when(cities) {
-                    "아시아" -> Glide.with(itemView).load(R.drawable.ic_asia3).into(cityImage)
-                    "유럽" -> Glide.with(itemView).load(R.drawable.ic_europe3).into(cityImage)
-                    "아메리카" -> Glide.with(itemView).load(R.drawable.ic_america3).into(cityImage)
-                    "아프리카" -> Glide.with(itemView).load(R.drawable.ic_africa3).into(cityImage)
+                    "서울" -> Glide.with(itemView).load(R.drawable.ic_seoul3).into(cityImage)
+                    "부산" -> Glide.with(itemView).load(R.drawable.ic_busan3).into(cityImage)
+                    "제주" -> Glide.with(itemView).load(R.drawable.ic_jeju3).into(cityImage)
                     else -> Glide.with(itemView).load(R.drawable.default_profile_img).into(cityImage)
                 }
                 QueryCityName.text = cities
-                QuerySpecificCityName.text = "해외 여행지"
+                QuerySpecificCityName.text = "국내 여행지"
             }
 
         }
